a7f02a167c9e6981d3d5d7913394b3038ac282f6
mitchelbade <mitchelm.bade@gmail.com>
2021-10-06 23:27:59 -0700

    commit tree d3fa2168640bfeb8b50f53ee00543a65682da8a3

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance){
   // Write your code here!
+
+  let message;
+
+  if (rideDistance <= 199) {
+    message = 'This one is on me!';
+  }
+  else if (rideDistance >= 2500) {
+    message = 'No can do.';
+  }
+  else if (rideDistance >= 2000) {
+    message = 'I will gladly take your thirty bucks.';
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+  let message;
+
+  message = city === 'NYC' ? "Ok, sounds good." : "No go.";
+
+  return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    case 'thanks for everything':
+      return 'Bye.'
+      break;
+  }
 }
